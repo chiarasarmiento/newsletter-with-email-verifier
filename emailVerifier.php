8b28eae6ad12a60d2faa754b9444418c87bfdc45
chiarasarmiento <142721196+chiarasarmiento@users.noreply.github.com>
2023-09-14 08:58:45 +0800

    Update emailVerifier.php

--- emailVerifier.php
+++ emailVerifier.php
@@ -12,11 +12,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
 
 $response = curl_exec($ch);
-
-if($response == "ok")
-	echo "success";
-else if($response == "key_not_valid")
-	echo "Oops! It looks like there's an issue with the form. Please try again.";
-else 
-	echo "Oops! the email you entered is invalid. Please try again.";
+echo $response;
+ 
 ?>
